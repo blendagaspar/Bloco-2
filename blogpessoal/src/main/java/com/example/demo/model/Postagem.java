87f43eaf888653493857641eedf94525d4d718b1
blendagaspar <blendagaspar@hotmail.com>
2021-07-07 14:31:11 -0300

    add classe usuario e classe security

--- blogpessoal/src/main/java/com/example/demo/model/Postagem.java
+++ blogpessoal/src/main/java/com/example/demo/model/Postagem.java
@@ -43,6 +43,11 @@ public class Postagem {
     private Tema tema;
 	
 	
+	@ManyToOne
+	@JsonIgnoreProperties ("postagem")
+	private Usuario usuario;
+	
+	
 	public long getId() {
 		return id;
 	}
@@ -76,5 +81,12 @@ public class Postagem {
 		this.tema = tema;
 	}
 	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	
 }
---
+++ blogpessoal/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,23 @@
+package com.example.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Usuario;
+
+
+
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <Usuario , Long>{
+
+	
+	public Optional<Usuario> findByUsuario(String usuario); //optional é porque pode devolver nulo caso não encontre o usuario
+	
+	
+	
+	
+}
